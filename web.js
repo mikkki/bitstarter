f2f5faf1b5ec7365799577ee19429bdcc8f1bb97
Iliana Toneva <iliana.toneva@pnmac.com>
2013-07-10 21:10:12 +0000

    changes

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = JSON.parse(fs.readFileSync(__dirname + '/index.html'));
-  response.send(buf.toString());
+  var txt = JSON.parse(fs.readFileSync('index.html'));
+  buf = new Buffer(256);
+  len = buf.write(txt, 0);
+  response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
